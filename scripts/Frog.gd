850b683507217dbfb36b17aadde920887d9df524
Clément Rivaille <rivaille.clement@orange.fr>
2023-07-08 00:51:53 +0200

    feat: platforms

--- scripts/Frog.gd
+++ scripts/Frog.gd
@@ -4,9 +4,17 @@ class_name Frog
 @export var VFORCE = 1000.0
 @export var MAX_HFORCE = 400.0
 
+@onready var joint: PinJoint2D = $PinJoint2D
+
 func _input(event: InputEvent) -> void:
   if event.is_action_pressed("ui_accept"):
     jump()
 
 func jump():
+  joint.node_b = "."
   apply_central_impulse(Vector2(randf_range(-MAX_HFORCE, MAX_HFORCE), -VFORCE))
+
+
+func _on_body_entered(body: Node) -> void:
+  if body is Platform:
+    joint.node_b = body.get_path()
---
+++ scripts/Platform.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+class_name Platform
+
+@export var min_pos := 0.0
+@export var max_pos := 1000.0
+
+var dragged := false
+var mouse_inside := false
+var pos_diff := 0.0
+
+func _input(event: InputEvent) -> void:
+  if event is InputEventMouseButton && mouse_inside:
+    dragged = event.is_pressed()
+    pos_diff = get_viewport().get_mouse_position().x - global_position.x
+
+func _process(_delta: float) -> void:
+  if dragged:
+    position.x = clampf(get_viewport().get_mouse_position().x - pos_diff, min_pos, max_pos)
+    pos_diff = get_viewport().get_mouse_position().x - global_position.x
+
+func _on_mouse_entered() -> void:
+  mouse_inside = true
+func _on_mouse_exited() -> void:
+  mouse_inside = false
+
